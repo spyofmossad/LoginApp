c64bb89dcfad143e61a4af03ca16506228d26767
dmitry.navitski <arbalestindigo@gmail.com>
2020-03-06 19:28:20 +0300

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Dmitry on 06.03.2020.
+//  Copyright © 2020 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
